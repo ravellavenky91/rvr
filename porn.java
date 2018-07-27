fa0b15881a546648587f5079d4e75d53a5d54d29
ravellavenky91 <40288538+ravellavenky91@users.noreply.github.com>
2018-07-27 14:44:11 +0530

    Create porn.java

---
+++ porn.java
@@ -0,0 +1,15 @@
+
+		Scanner type=new Scanner(System.in);
+		int  p=type.nextInt();
+		if(p>=0)
+		{
+			System.out.println("Positive");
+		}
+		else if(p>=0)
+		{
+			System.out.println("Negative");
+		}
+		else
+		{
+			System.out.println("Zero");
+		}
