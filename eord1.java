bf8590b6aa0c8cf20bfdd664fba66e5c0a6f85d7
ravellavenky91 <40288538+ravellavenky91@users.noreply.github.com>
2018-07-27 15:25:38 +0530

    Create eord1.java

---
+++ eord1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner type=new Scanner(System.in);
+		int r=type.nextInt();
+		if(r%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+	    System.out.println("Odd");
+		}
+	}
+}
