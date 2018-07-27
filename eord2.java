d232f17142ec5d8c22bf90d95269884f7e71dd48
ravellavenky91 <40288538+ravellavenky91@users.noreply.github.com>
2018-07-27 15:33:48 +0530

    Create eord2.java

---
+++ eord2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner type=new Scanner(System.in);
+		int r=type.nextInt();
+		if(r%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+	    System.out.println("Odd");
+		}
+	}
+}
